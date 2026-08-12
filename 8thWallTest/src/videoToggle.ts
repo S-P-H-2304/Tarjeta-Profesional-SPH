c56711d27720ed17a8bd0902743818449c5c7615
S-P-H-2304 <sebastian.perezh2304@gmail.com>
2026-08-12 11:49:59 -0500

    Función restart
    Se agregó el ícono de restart y la funcionalidad para testear

--- 8thWallTest/src/videoToggle.ts
+++ 8thWallTest/src/videoToggle.ts
@@ -3,34 +3,63 @@ import * as ecs from '@8thwall/ecs'
 ecs.registerComponent({
   name: 'video',
   schema: {
-    button: ecs.eid,
+    playPauseButton: ecs.eid,
+    restartButton: ecs.eid,
     video: ecs.eid,
-    icon: ecs.eid,
+    playIcon: ecs.eid,
+    restartIcon: ecs.eid,
   },
   stateMachine: ({world, eid, schemaAttribute}) => {
-    const {button, video, icon} = schemaAttribute.get(eid)
+    const {playPauseButton, restartButton, video, playIcon, restartIcon} = schemaAttribute.get(eid)
+
+    let isPaused = false   // el video arranca reproduciéndose
+    let hasEnded = false   // si llegó al final de forma natural
+
+    const updateIcons = () => {
+      ecs.Ui.mutate(world, playIcon, (cursor) => {
+        cursor.opacity = isPaused ? 1 : 0
+        return false
+      })
+      ecs.Ui.mutate(world, restartIcon, (cursor) => {
+        cursor.opacity = isPaused ? 1 : 0
+        return false
+      })
+    }
 
     ecs.defineState('default')
       .initial()
       .onEnter(() => {
-        ecs.Ui.mutate(world, icon, (cursor) => {
-          cursor.opacity = 0
-          return false
-        })
+        updateIcons()
       })
-      .listen(button, ecs.input.UI_CLICK, () => {
-        let isPaused = false
-
-        ecs.VideoControls.mutate(world, video, (controls) => {
-          controls.paused = !controls.paused
-          isPaused = controls.paused
+      .listen(playPauseButton, ecs.input.UI_CLICK, () => {
+        if (hasEnded) {
+          // si ya terminó, "reproducir" equivale a reiniciar
+          ecs.video.setCurrentTime(world, video, 0)
+          hasEnded = false
+        }
+        isPaused = !isPaused
+        ecs.VideoControls.mutate(world, video, (cursor) => {
+          cursor.paused = isPaused
           return false
         })
+        updateIcons()
+      })
+      .listen(restartButton, ecs.input.UI_CLICK, () => {
+        if (!isPaused) return // el video se está reproduciendo: ignora el toque
 
-        ecs.Ui.mutate(world, icon, (cursor) => {
-          cursor.opacity = isPaused ? 1 : 0
+        ecs.video.setCurrentTime(world, video, 0)
+        hasEnded = false
+        isPaused = false
+        ecs.VideoControls.mutate(world, video, (cursor) => {
+          cursor.paused = false
           return false
         })
+        updateIcons()
+      })
+      .listen(video, ecs.events.VIDEO_END, () => {
+        isPaused = true
+        hasEnded = true
+        updateIcons()
       })
   },
 })
